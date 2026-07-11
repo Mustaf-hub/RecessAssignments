dceeec7ea450eed0c88705f76d828582ca4bd581
Mustaf10 <xaviermustaf1010@gmail.com>
2026-07-11 12:38:58 +0300

    added

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html',title='Sign In')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html',title='Sign Up')
